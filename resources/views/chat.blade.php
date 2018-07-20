8949d96a0f6a2b0e35ceaab7e052a98d9848335b
kabbenhuis <kevin.abbenhuis@gmail.com>
2018-07-20 08:52:28 +0200

    + Test opdracht git commit

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Chats</div>
+
+                    <div class="panel-body">
+                        <chat-messages :messages="messages"></chat-messages>
+                    </div>
+                    <div class="panel-footer">
+                        <chat-form
+                                v-on:messagesent="addMessage"
+                                v-on:typing="isTyping"
+                                :user="{{ Auth::user() }}"
+                        ></chat-form>
+
+                    </div>
+                </div>
+                <span v-show="typing" class="help-block" style="font-style: italic;">
+                    Someone is typing...
+                </span>
+            </div>
+        </div>
+    </div>
+@endsection
